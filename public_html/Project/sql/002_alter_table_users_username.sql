4722bd5e052574c42396cb2850e509e968c1818b
Ramanand <rmk7@njit.edu>
2022-12-22 17:13:48 +0500

    .env updates with clear db heroku

--- public_html/Project/sql/002_alter_table_users_username.sql
+++ public_html/Project/sql/002_alter_table_users_username.sql
@@ -1,3 +1,3 @@
 ALTER TABLE Users ADD COLUMN username varchar(30) 
-not null unique default (substring_index(email, '@', 1)) 
+not null unique
 COMMENT 'Username field that defaults to the name of the email given';
