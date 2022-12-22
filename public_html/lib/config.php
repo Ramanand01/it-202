c7b16ee4e03e70409e1384d67e7f6c0e787bc3c2
Ramanand <rmk7@njit.edu>
2022-12-22 18:26:04 +0500

    connection updated

--- public_html/lib/config.php
+++ public_html/lib/config.php
@@ -9,8 +9,8 @@ else{
     //load from heroku env variables
     $db_url      = parse_url(getenv("DB_URL"));
 }
-$dbhost   = $db_url["host"];
-$dbuser = $db_url["user"];
-$dbpass = $db_url["pass"];
-$dbdatabase       = substr($db_url["path"],1);
+$dbhost   = 'us-cdbr-east-06.cleardb.net';
+$dbuser = 'bf5c9c52787627';
+$dbpass = '0031a94e';
+$dbdatabase       = 'heroku_5d4f3ad9672cb2d';
 ?>
